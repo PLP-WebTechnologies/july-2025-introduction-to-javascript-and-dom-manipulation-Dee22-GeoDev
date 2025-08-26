0ad8909d3053e28daeb64de8ddba19949f8ab96b
Dee-GeoDev22 <moyadamacline@gmail.com>
2025-08-26 11:33:13 +0300

    Added my week 5 assignment

---
+++ script.js
@@ -0,0 +1,48 @@
+let userAge = 24; 
+if (userAge >= 18) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are a minor.");
+}
+
+let a = 5, b = 10;
+console.log("Sum of a + b =", a + b);
+
+function calculateTotal(price, quantity) {
+  return price * quantity;
+}
+console.log("Total =", calculateTotal(50, 3));
+
+function greetUser(name) {
+  return "Hello, " + name + "! Welcome to JavaScript.";
+}
+console.log(greetUser("Damacline"));
+
+let fruits = ["Apple", "Banana", "Mango"];
+for (let i = 0; i < fruits.length; i++) {
+  console.log("Fruit:", fruits[i]);
+}
+
+let count = 5;
+while (count > 0) {
+  console.log("Countdown:", count);
+  count--;
+}
+
+let newDiv = document.createElement("div");
+newDiv.innerText = "I was created by JavaScript!";
+document.body.appendChild(newDiv);
+
+document.getElementById("changeTextBtn").addEventListener("click", function() {
+  document.getElementById("title").innerText = "Hey things are getting tougher!";
+});
+
+document.getElementById("toggleColorBtn").addEventListener("click", function() {
+  document.getElementById("title").classList.toggle("blue");
+});
+
+document.getElementById("createItemBtn").addEventListener("click", function() {
+  let li = document.createElement("li");
+  li.innerText = "New Item " + (document.querySelectorAll("#dynamicList li").length + 1);
+  document.getElementById("dynamicList").appendChild(li);
+});
